db98bf721a21ed03c8ad4b779eb3ad72a4962b8c
Don Spire <don.spire1@gmail.com>
2021-10-08 06:01:20 -0700

    add TechSelect component

---
+++ src/components/techs/TechSelectOptions.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getTechs } from "../../actions/techActions";
+
+const TechSelectOptions = ({ getTechs, tech: { techs, loading } }) => {
+  useEffect(() => {
+    getTechs();
+  }, []);
+
+  return (
+    !loading &&
+    techs !== null &&
+    techs.map((tech) => (
+      <option key={tech.id} value={`${tech.firstName} ${tech.lastName}`}>
+        {tech.firstName}
+        {tech.lastName}{" "}
+      </option>
+    ))
+  );
+};
+
+const mapStateToProps = (state) => ({
+  tech: state.tech,
+});
+
+export default connect(mapStateToProps, { getTechs })(TechSelectOptions);
