d4e1128a678af5a170c1fc713e5cff51421371fe
Don Spire <don.spire1@gmail.com>
2021-10-08 05:18:43 -0700

    add Redux to SearchBar

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { connect } from "react-redux";
+import { searchLogs } from "../../actions/logActions";
 
-const SearchBar = () => {
+const SearchBar = ({ searchLogs }) => {
+  const text = useRef("");
+
+  // no onSubmit to continuously search and call backend
+  const onChange = (e) => {
+    searchLogs(text.current.value);
+  };
+
   return (
     <nav style={{ marginBottom: "30px" }} className="blue">
       <div className="nav-wrapper">
         <form>
           <div className="input-field">
-            <input id="search" type="search" />
+            <input
+              id="search"
+              type="search"
+              placeholder="Search Logs..."
+              ref={text}
+              onChange={onChange}
+            />
             <label className="label-icon" htmlFor="search">
               <i className="material-icons">search</i>
             </label>
@@ -18,4 +33,4 @@ const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+export default connect(null, { searchLogs })(SearchBar);
