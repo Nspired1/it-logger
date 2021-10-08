13b3cbd9a2d5acd6f8b0899f8c22ae3f52ff0e58
Don Spire <don.spire1@gmail.com>
2021-10-07 19:08:27 -0700

    add initial components

---
+++ src/components/logs/Logs.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import LogItem from "./LogItem";
+import Preloader from "../layout/Preloader";
+
+const Logs = () => {
+  const [logs, setLogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getLogs();
+  }, []);
+
+  const getLogs = async () => {
+    setLoading(true);
+    const res = await fetch(`/logs`);
+    const data = await res.json();
+
+    setLogs(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  return (
+    <div>
+      <ul className="collection with-header">
+        <li className="collection-header">
+          <h4 className="center">System Logs</h4>
+        </li>
+        {!loading && logs.length === 0 ? (
+          <p className="center">No logs to show... </p>
+        ) : (
+          logs.map((log) => <LogItem log={log} key={log.id} />)
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Logs;
